1e868984d973b3f1aa73ad817e16dec00f3362b3
Witold Kupś <witold.kups@interlan.pl>
2018-10-20 06:02:35 +0200

    Add average statistic

--- src/main/scala/cs/put/pmds/labs/Main.scala
+++ src/main/scala/cs/put/pmds/labs/Main.scala
@@ -2,6 +2,7 @@ package cs.put.pmds.labs
 
 import java.util.concurrent.ThreadLocalRandom
 
+import scala.collection.parallel.ParSeq
 import scala.io.StdIn
 
 case class ProgramArgs(
@@ -49,7 +50,7 @@ object CmdArgsSupplier extends ProblemArgsSupplier {
 }
 
 object ProvidedArgsSupplier extends ProblemArgsSupplier {
-	override def produce() = ProgramArgs(100, 0.5, 10, 100)
+	override def produce() = ProgramArgs(100, 0.1, 10, 100)
 }
 
 trait ProblemArgsSupplier {
@@ -64,21 +65,21 @@ case class TerroristsPairs(
 	override def toString: String = s"TerroristPairs($t1, $t2, ${meetings.mkString("[", ",", "]")}"
 }
 
-class NightsCreator(private val supplier: ProblemArgsSupplier) {
+class NightsCreator(private val programArgs: ProgramArgs) {
 	private def getPersonNights(person: Person): Array[NightInTheHotel] = {
 		val random = ThreadLocalRandom.current()
 		days.withFilter(_ => random.nextDouble() <= programArgs.probabilityOfSleepingInHotel)
 		 .map(d => NightInTheHotel(person, Night(hotels(random.nextInt(hotels.length - 1)), d)))
 	}
 
-	private val programArgs = supplier.produce()
 	private val persons = (1 to programArgs.numOfPersons).map(Person).toArray
 	private val hotels = (1 to programArgs.numOfHotels).map(Hotel).toArray
 	private val days = (1 to programArgs.numOfDays).toArray
-	val nights: Array[NightInTheHotel] = persons.flatMap(getPersonNights _)
+	val nights: Array[NightInTheHotel] = persons.flatMap(getPersonNights)
 }
 
 object Util {
+
 	implicit class Col[T](obj: Array[T]) {
 		def cross(): Array[(T, T)] = {
 			obj
@@ -86,45 +87,26 @@ object Util {
 			 .filter(p => p._1 != p._2)
 		}
 	}
+
 }
 
 class TerroristFinder(private val nights: Array[NightInTheHotel]) {
-
-	import Util._
-
-	private def isPairInTheSameNightAndHotel(p: (NightInTheHotel, NightInTheHotel)) = {
+	private def isPairInTheSameNightAndHotel(p: (NightInTheHotel, NightInTheHotel)) =
 		p._1.person != p._2.person && p._1.night == p._2.night
-	}
-
-	def getTerroristsNightsPairs(pairNights: Array[TerroristsPairs]) = {
-		pairNights.flatMap(extractNightsPairs)
-	}
 
 	def getPairNights() = {
-		nights.toStream
+		nights.toStream.par
 		 .flatMap(n1 => nights.toStream.map(n2 => (n1, n2)))
 		 .filter(isPairInTheSameNightAndHotel)
 		 .groupBy(pair)
 		 .mapValues(distinctNights)
 		 .filter(p => p._2.length > 1)
-		 .toArray
 		 .map(p => TerroristsPairs(p._1._1, p._1._2, p._2))
+		 .toArray
 	}
 
-	private def extractNightsPairs(p: TerroristsPairs) = {
-		p.meetings.cross()
-		 .map(sort)
-		 .distinct
-		 .map(n => NightlyPair(p.t1, p.t2, n._1, n._2))
-	}
-
-	private def sort(t: (Night, Night)): (Night, Night) = {
-		if (t._1.day < t._2.day) (t._1, t._2) else (t._2, t._1)
-	}
-
-	private def distinctNights(v: Stream[(NightInTheHotel, NightInTheHotel)]) = {
+	private def distinctNights(v: ParSeq[(NightInTheHotel, NightInTheHotel)]) =
 		v.map(_._1.night).distinct.toArray
-	}
 
 	private def pair(p: (NightInTheHotel, NightInTheHotel)): (Person, Person) = {
 		val p1 = p._1.person
@@ -133,14 +115,13 @@ class TerroristFinder(private val nights: Array[NightInTheHotel]) {
 	}
 }
 
-class HistogramMaker(private val pairs: Array[TerroristsPairs]) {
+object HistogramMaker {
 
 	import plotly._
 	import Plotly._
 
-	def make(): Unit = {
-		val results = pairs.groupBy(_.meetings.length).mapValues(_.length).toArray
-		val (x, y) = results.toSeq.unzip
+	def make(stats: TerroristsStats): Unit = {
+		val (x, y) = stats.terroristsMeetingsCount.toSeq.unzip
 		Bar(x, y).plot(
 			title = "Pairs meetings to frequency",
 			height = 950,
@@ -149,13 +130,74 @@ class HistogramMaker(private val pairs: Array[TerroristsPairs]) {
 	}
 }
 
-object Main extends App {
+case class TerroristsStats(
+	terroristsMeetingsCount: Array[(Int, Double)],
+	mediumNumberOfPotentialTerrorists: Double,
+	mediumNumberOfPersonoNightPairs: Double
+)
 
-	private val finder = new TerroristFinder(new NightsCreator(ProvidedArgsSupplier).nights)
-	private val terrorists = finder.getPairNights()
-	println(s"number of all potential pairs: ${terrorists.length}")
-	private val nightsByPair = finder.getTerroristsNightsPairs(terrorists)
-	println(s"number of terrorist pairs and nights pairs (persono-night-pair): ${nightsByPair.length}")
-	private val maker = new HistogramMaker(terrorists)
-	maker.make()
+
+class TerroristsStatisticMaker {
+
+	import Util._
+
+	private var iterations = 0d
+	private var stats = Map[Int, Int]()
+	private var personoNightsPairs = 0
+
+	def add(terroristsPairs: Array[TerroristsPairs]): Unit = {
+		println(s"number of all potential pairs: ${terroristsPairs.length}")
+		addPersonoNightPairStat(terroristsPairs)
+		addOccurrenceOfPairMeetingStats(terroristsPairs)
+		iterations += 1
+	}
+
+	private def addOccurrenceOfPairMeetingStats(terroristsPairs: Array[TerroristsPairs]): Unit = {
+		val newOne = terroristsPairs.groupBy(_.meetings.length).mapValues(_.length)
+		stats = (newOne.keySet ++ stats.keySet).map(k => (k, stats.getOrElse(k, 0) + newOne.getOrElse(k, 0))).toMap
+	}
+
+	private def addPersonoNightPairStat(terroristsPairs: Array[TerroristsPairs]) = {
+		val nightsByPair = getTerroristsNightsPairs(terroristsPairs)
+		val personoNightsPairsFromPair = nightsByPair.length
+		println(s"number of terrorist pairs and nights pairs (persono-night-pair): $personoNightsPairsFromPair")
+		personoNightsPairs += personoNightsPairsFromPair
+	}
+
+	def getStats(): TerroristsStats = {
+		val mediumNumberOfTerrorists = stats.map(t => t._2 / iterations).sum
+		println(s"medium number of potential pairs: $mediumNumberOfTerrorists")
+		val personoNightPairsMediumNumber = personoNightsPairs / iterations
+		println(s"medium number of persono-night-pairs: ${personoNightPairsMediumNumber}")
+		TerroristsStats(
+			stats.mapValues(_ / iterations).toArray,
+			mediumNumberOfTerrorists,
+			personoNightPairsMediumNumber
+		)
+	}
+
+	def getTerroristsNightsPairs(pairNights: Array[TerroristsPairs]) =
+		pairNights.flatMap(extractNightsPairs)
+
+	private def extractNightsPairs(p: TerroristsPairs) = {
+		p.meetings.cross()
+		 .map(sort)
+		 .distinct
+		 .map(n => NightlyPair(p.t1, p.t2, n._1, n._2))
+	}
+
+	private def sort(t: (Night, Night)): (Night, Night) =
+		if (t._1.day < t._2.day) (t._1, t._2) else (t._2, t._1)
+}
+
+object Main extends App {
+	private val tries = 10
+	private val arg = ProvidedArgsSupplier.produce()
+	private val statisticMaker = new TerroristsStatisticMaker()
+	for (_ <- 0 until tries) {
+		val finder = new TerroristFinder(new NightsCreator(arg).nights)
+		val terrorists = finder.getPairNights()
+		statisticMaker.add(terrorists)
+	}
+	HistogramMaker.make(statisticMaker.getStats())
 }
