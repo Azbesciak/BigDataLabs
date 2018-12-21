53921b689a4459b526c1742e95a2ca5d882288ab
Witold Kupś <witold.kups@interlan.pl>
2018-12-21 23:00:21 +0100

    Add task for lab 9

---
+++ lab9/src/main/kotlin/cs/put/pmds/lab9/Main.kt
@@ -0,0 +1,99 @@
+package cs.put.pmds.lab9
+
+import com.carrotsearch.hppc.IntHashSet
+import java.io.BufferedWriter
+import java.io.File
+import java.math.RoundingMode
+import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.stream.Stream
+import kotlin.system.measureTimeMillis
+
+fun main(args: Array<String>) {
+    require(args.size == 2) { "usage: <input path> <output path>" }
+    val (input, output) = args
+    val file = File(input).also {
+        require(it.exists()) { "file does not exist" }
+    }
+    val mem = Runtime.getRuntime().freeMemory()
+    val measuredTime = measureTimeMillis {
+        val userSongs = file.useLines { lines ->
+            lines.drop(1)
+                    .map(::mapLine)
+                    .filter { it[0] != it[1] }
+                    .groupBy { it.first() }
+                    .mapValues {
+                        val s = IntHashSet(it.value.size)
+                        it.value.forEach { s.add(it[1]) }
+                        s.toArray().apply { sort() }
+                    }.toList()
+
+        }
+        println("used mem: ${mem - Runtime.getRuntime().freeMemory()} songs: ${userSongs.size}")
+        val counter = AtomicInteger(0)
+        val step = userSongs.size / 1000
+        val progress = AtomicInteger(0)
+
+        val neighbors = userSongs
+                .parallelStream()
+                .peek {
+                    if (counter.incrementAndGet() % step == 0) print("progress: ${progress.incrementAndGet() / 10.0}%\r")
+                }
+                .map { user -> user.first to findClosestNeighbours(userSongs, user) }
+
+        File(output)
+                .also { it.parentFile.mkdirs() }
+                .outputStream()
+                .bufferedWriter()
+                .use { o -> o writeUserNeighbours neighbors }
+    }
+
+    println("total time: $measuredTime")
+
+}
+
+private infix fun BufferedWriter.writeUserNeighbours(neighbors: Stream<Pair<Int, Stream<Pair<Int, Double>>>>) {
+    neighbors.forEach { (user, neigh) ->
+        write("User = $user\n")
+        neigh.forEach { (u, jac) ->
+            write("\t$u ${jac.toBigDecimal().setScale(4, RoundingMode.HALF_UP)}\n")
+        }
+        newLine()
+    }
+}
+
+private fun findClosestNeighbours(userSongs: List<Pair<Int, IntArray>>, user: Pair<Int, IntArray>) =
+        userSongs
+                .stream()
+                .map { other ->
+                    if (user.first == other.first) other.first to 0.0
+                    else other.first to jaccardCoef(user.second, other.second)
+                }.filter { it.second > 0 }
+                .limit(100)
+
+private fun jaccardCoef(user: IntArray, other: IntArray): Double {
+    if (user.first() > other.last() || user.last() < other.first()) return 0.0
+    var common = 0
+    var onlyUser = 0.0
+    val userIterator = user.iterator()
+    val otherIterator = other.iterator()
+    var currentUser = userIterator.next()
+    var currentOther = otherIterator.next()
+    while (userIterator.hasNext() && otherIterator.hasNext()) {
+        when {
+            currentUser == currentOther -> {
+                common++
+                currentUser = userIterator.next()
+                currentOther = otherIterator.next()
+            }
+            currentUser > currentOther -> currentOther = otherIterator.next()
+            currentUser < currentOther -> {
+                onlyUser++
+                currentUser = userIterator.next()
+            }
+        }
+    }
+    return common / (other.size + onlyUser)
+}
+
+private fun mapLine(it: String) = it.split(",").map { it.trim().toInt() }
